0d7f25539dfd10c360ddae68f419a550cf84fbae
Eduard Ereza Martínez <eduard@ereza.cat>
2022-05-29 19:04:14 +0200

    Update copyrights

--- sample/src/main/java/cat/ereza/logcatreporter/sample/SampleCrashingApplication.java
+++ sample/src/main/java/cat/ereza/logcatreporter/sample/SampleCrashingApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Eduard Ereza Martínez
+ * Copyright (c) 2017-2022 Eduard Ereza Martínez <eduard@ereza.cat>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
